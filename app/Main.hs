b004c95b06eb328cd6a50f9bcea5b8a22a10b345
Ruihan Wang <wang_ruihan@hotmail.com>
2019-03-05 21:29:39 -0500

    example adder circuit

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,27 @@
+{-# LANGUAGE TypeApplications #-}
+
 module Main where
 
-import Lib
+import Circuit.Gates ((<+>))
+import Circuit.Wire
+import Circuit (eval, getCircuit)
+import System.Environment (getArgs)
+import qualified Data.ByteString.Char8 as BS
+import Control.Monad.Trans (liftIO)
+import Data.Word (Word8)
 
 main :: IO ()
-main = someFunc
+main = do
+  args <- getArgs
+  case args of
+    (x : y : xs) -> do
+      circuit <- getCircuit adderC
+      print circuit
+      result <- eval @Int (read x :: Int) (read y :: Int) adderC
+      print result
+    _ -> print "Please give two numbers"
+ where
+  adderC = do
+    a <- in64
+    b <- in64
+    a <+> b
