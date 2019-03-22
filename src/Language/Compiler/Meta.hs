6fa8898261e329c64d3b26ac2e4d01eb63885be1
Ruihan Wang <wang_ruihan@hotmail.com>
2019-03-21 22:47:48 -0400

    native Haskell code generation

---
+++ src/Language/Compiler/Meta.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE TemplateHaskell #-}
+
+module Language.Compiler.Meta (T (..)) where
+
+import Language.Haskell.TH
+import Language.Core
+import qualified Data.Function as F (fix)
+
+newtype T st a = T { unT :: Q Exp }
+
+instance Core T where
+  int n = T [| n |]
+  bool b = T [| b |]
+  a .+ b = T [| $(unT a) + $(unT b) |]
+  a .- b = T [| $(unT a) - $(unT b) |]
+  a .* b = T [| $(unT a) * $(unT b) |]
+  lam f  = T $ lamE [varP (mkName "x")] (unT $ f (T $ varE (mkName "x")))
+  app f a = T $ appE (unT f) (unT a)
+  fix f = T [| F.fix $(unT (lam f)) |]
+  if_ p a b = T [| if $(unT p) then $(unT a) else $(unT b) |]
+  a .<= b = T [| $(unT a) <= $(unT b) |]
