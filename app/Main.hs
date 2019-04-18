9d8bdd8a72f04085f4d3b1eb6eae7e344ba18c6f
Ruihan Wang <wang_ruihan@hotmail.com>
2019-04-17 21:06:20 -0400

    update example

--- app/Main.hs
+++ app/Main.hs
@@ -1,21 +1,24 @@
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE MultiWayIf #-}
 
 module Main where
 
 import           Circuit
 import           Circuit.Gates
+import           Circuit.Parser
 import           Circuit.Wire
-import           Control.Monad.Trans       (liftIO)
-import qualified Data.ByteString           as BS
-import qualified Data.ByteString.Char8     as BSC
-import           Data.List.Split           (chunksOf)
-import           Data.Word                 (Word8)
+import           Control.Monad.Trans        (liftIO)
+import qualified Data.ByteString            as BS
+import qualified Data.ByteString.Char8      as BSC
+import qualified Data.ByteString.Lazy.Char8 as LBSC
+import           Data.List.Split            (chunksOf)
+import           Data.Word                  (Word8)
 import           Language.Compiler.Circuit
 import           Language.Core
 import           Language.Extension
 import           Network.Pair
-import           System.Environment        (getArgs)
+import           System.Environment         (getArgs)
 import           System.ZMQ4.Monadic
 
 main :: IO ()
@@ -38,16 +41,12 @@ main = do
               (y : []) -> BSC.pack y
         in  runZMQ $ do
               sock <- initClient "tcp://127.0.0.1:1145"
-              runClient y' sock
+              hex  <- runClient y' sock
+              liftIO . putStrLn . LBSC.unpack $ hex
         -- y' = BS.pack . fromBits . finiteToBits $ (read y :: Int)
     --   in  do
     --         str <- eval x' y' prog
     --         print . fromFiniteBits @Int . toBits . BS.unpack $ str
     _ -> print "Please give two numbers"
- where
-  prog = do
-    a <- in64 Alice
-    b <- in64 Bob
-    runWith a b
-      $ leven (fmap word8 (BS.unpack "ab")) (fmap word8 (BS.unpack "bd"))
+  where prog = parseRun "/home/cirno/Downloads/AES-non-expanded.txt"
 
